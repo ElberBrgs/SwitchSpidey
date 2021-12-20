5cc167444398c6ab7645db946ce5516b27c44bdf
ElberBrgs <elberborgess@gmail.com>
2021-12-19 23:25:41 -0300

    :grey_exclamation: First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var valor = document.querySelector("#texto");
+
+var btn = document
+  .querySelector(".btn")
+  .addEventListener("click", function (evt) {
+    switch (valor.value) {
+      case "Tobey Maguire" || "Tobey":
+        document.write("<h2>Esse é o primeiro Homem Aranha!</h2>");
+        break;
+      case "Andrew Garfield" || "Andrew":
+        document.write("<h2>Esse é o segundo Homem Aranha!!</h2>");
+        break;
+      case "Tom Holland" || "Tom":
+        document.write("<h2>Esse é o terceiro Homem Aranha!!!</h2>");
+        break;
+      default:
+        document.write(
+          `<h2>Não é um dos amigões da vizinhança nos cinemas...</h2> `
+        );
+    }
+  });
