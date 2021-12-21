060fcbb8509389b6e7f3b2569bd6a6cce8c3360a
ElberBrgs <elberborgess@gmail.com>
2021-12-21 20:28:22 -0300

    Fixed :right_anger_bubble:

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ var btn = document
   .querySelector(".btn")
   .addEventListener("click", function (evt) {
     switch (valor.value) {
-      case "Tobey Maguire" || "Tobey":
+      case "Tobey Maguire":
+      case "Tobey":
         document.write("<h2>Esse é o primeiro Homem Aranha!</h2>");
         break;
-      case "Andrew Garfield" || "Andrew":
+      case "Andrew Garfield":
+      case "Andrew":
         document.write("<h2>Esse é o segundo Homem Aranha!!</h2>");
         break;
-      case "Tom Holland" || "Tom":
+      case "Tom Holland":
+      case "Tom":
         document.write("<h2>Esse é o terceiro Homem Aranha!!!</h2>");
         break;
       default:
